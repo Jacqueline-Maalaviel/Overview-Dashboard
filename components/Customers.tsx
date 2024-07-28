b5f17fc6592a5c6d3cbd41613294eecb380ee3a2
Jacqueline Maalaviel <jacquelinemvl20@gmail.com>
2024-07-28 17:19:14 +0000

    pie chart

--- components/Customers.tsx
+++ components/Customers.tsx
@@ -1,82 +1,34 @@
-import React, { useState } from "react";
-import { PieChart, Pie, Sector, ResponsiveContainer } from "recharts";
+import React from "react";
+import { PieChart, Pie, Tooltip } from "recharts";
+
+type data = [
+  {name: string, value: number}
+]
 
 const data = [
-  { name: 'New Customers', value: 170 },
-  { name: 'Returning Customers', value: 300 }
+  {name: "New Costumers", value: 400 },
+  {name: "Returning Customers", value: 378}
 ];
 
-const renderActiveShape = (props) => {
-  const RADIAN = Math.PI / 180;
-  const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
-  const sin = Math.sin(-RADIAN * midAngle);
-  const cos = Math.cos(-RADIAN * midAngle);
-  const sx = cx + (outerRadius + 10) * cos;
-  const sy = cy + (outerRadius + 10) * sin;
-  const mx = cx + (outerRadius + 30) * cos;
-  const my = cy + (outerRadius + 30) * sin;
-  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-  const ey = my;
-  const textAnchor = cos >= 0 ? 'start' : 'end';
 
-  return (
-    <g>
-      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={"#808080"}>
-        {`Customers`}
-      </text>
-      <Sector
-        cx={cx}
-        cy={cy}
-        innerRadius={innerRadius}
-        outerRadius={outerRadius}
-        startAngle={startAngle}
-        endAngle={endAngle}
-        fill={fill}
-      />
-      <Sector
-        cx={cx}
-        cy={cy}
-        startAngle={startAngle}
-        endAngle={endAngle}
-        innerRadius={outerRadius + 6}
-        outerRadius={outerRadius + 10}
-        fill={fill}
-      />
-      <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
-      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`${payload.name}: ${value}`}</text>
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
-        {`(Rate ${(percent * 100).toFixed(2)}%)`}
-      </text>
-    </g>
-  );
-};
 
 const Customers = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const onPieEnter = (_, index) => {
-    setActiveIndex(index);
-  };
-
+  
+  
   return (
     <div className='w-full md:col-span-1 lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white flex justify-between border rounded-lg'>
-      <ResponsiveContainer width="100%" height="100%">
-        <PieChart width={100} height={100}>
-          <Pie
-            activeIndex={activeIndex}
-            activeShape={renderActiveShape}
-            data={data}
-            cx="50%"
-            cy="50%"
-            innerRadius={60}
-            outerRadius={80}
-            fill="#bb8fce"
-            dataKey="value"
-            onMouseEnter={onPieEnter}
-          />
-        </PieChart>
-      </ResponsiveContainer>
+      <PieChart width={1000} height={400}>
+        <Pie
+          dataKey="value"
+          isAnimationActive={false}
+          data={data}
+          cx={200}
+          cy={200}
+          fill="#e6b0aa"
+          label />
+
+        <Tooltip />
+      </PieChart>
     </div>
   );
 };
